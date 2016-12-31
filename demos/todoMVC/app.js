7c48d7b5a8eeeb41c5aae2c4a01589a0eb0abbed
veah <391821744@qq.com>
2016-12-31 23:35:20 +0800

    @dblclick NOT @dbclick

--- demos/todoMVC/app.js
+++ demos/todoMVC/app.js
@@ -8,7 +8,7 @@ var todoStorage = {
         return todos
     },
     save(todos){
-        localStorage.setItem(STORAGE_KEY,JSON.STRINGIFY(TODOS))
+        localStorage.setItem(STORAGE_KEY,JSON.stringify(todos))
     }
 }
 
