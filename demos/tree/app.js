c28a6fb019cec3b54f547728e96c5bcaeb24920d
veah <391821744@qq.com>
2017-01-31 23:39:21 +0800

    tree done

---
+++ demos/tree/app.js
@@ -0,0 +1,73 @@
+var data = {
+    name: 'my tree',
+    children: [
+        {name: 'hello'},
+        {name: 'mac'},
+        {
+            name: 'child folder',
+            children: [
+                {
+                    name: 'child folder',
+                    children: [
+                        {name: 'hello'},
+                        {name: 'mac'}
+                    ]
+                },
+                {name: 'hello'},
+                {name: 'mac'},
+                {
+                    name: 'child folder',
+                    children: [
+                        {name: 'hello'},
+                        {name: 'mac'}
+                    ]
+                }
+            ]
+        }
+    ]
+}
+
+// define the item component
+Vue.component('item',{
+    template: '#item-template',
+    props: {
+        model: Object
+    },
+    data: function(){
+        return {
+            open: false
+        }
+    },
+    computed: {
+        isFolder: function(){
+            return this.model.children && this.model.children.length
+        }
+    },
+    methods: {
+        toggle: function(){
+            if(this.isFolder) {
+                this.open = !this.open
+            }
+        },
+        changeType: function(){
+            if(!this.isFolder){
+                Vue.set(this.model,'children',[])
+                this.addChild()
+                this.open = true
+            }
+        },
+        addChild: function(){
+            this.mdoel.children.push({
+                name: 'new stuff'
+            })
+        }
+    }
+})
+
+// boot up the demo
+var demo = new Vue({
+    el: '#demo',
+    data: {
+        treeData:data
+    }
+})
