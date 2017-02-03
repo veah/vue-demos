e5a95e0e861e06defb25167c7c4873d939a1698a
veah <391821744@qq.com>
2017-02-03 11:46:22 +0800

    js completed

---
+++ demos/modal/app.js
@@ -0,0 +1,12 @@
+// register modal component
+Vue.component('modal',{
+    template:'#modal-template'
+})
+
+// init root app
+new Vue({
+    el:'app',
+    data: {
+        showModal: false
+    }
+})
