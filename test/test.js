fc3369c95a9194ebfac13ac091eb216fcfc34416
veah <391821744@qq.com>
2017-02-09 11:40:03 +0800

    transition test

--- test/test.js
+++ test/test.js
@@ -12,3 +12,25 @@ new Vue({
     }
   }
 })
+
+new Vue({
+  el: '#list-complete-demo',
+  data: {
+    items: [1,2,3,4,5,6,7,8,9],
+    nextNum: 10
+  },
+  methods: {
+    randomIndex: function () {
+      return Math.floor(Math.random() * this.items.length)
+    },
+    add: function () {
+      this.items.splice(this.randomIndex(), 0, this.nextNum++)
+    },
+    remove: function () {
+      this.items.splice(this.randomIndex(), 1)
+    },
+    shuffle: function () {
+      this.items = _.shuffle(this.items)
+    }
+  }
+})
